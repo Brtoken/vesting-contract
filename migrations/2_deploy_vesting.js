afee99dac40695184059644eb728e1d5cb7aab5b
dezzyboy <dezzyboy@tradebit.ca>
2021-08-20 14:09:41 -0400

    scnds

--- migrations/2_deploy_vesting.js
+++ migrations/2_deploy_vesting.js
@@ -5,7 +5,7 @@ const toBN = web3.utils.toBN;
 
 module.exports = async function (deployer, accounts) {
   const advisor = "0x2Ca91f02747d139b228DfF8e35B8D02aa276C93a";
-  const brToken = "0x03476207EF6791CB0D021C5F9225bB9d70BC5fb5";
+  const brToken = "0xC9574679a89F9893E9b1e288ce6Aad1C7BE60624";
   await deployer.deploy(Vesting, brToken, advisor, {gas: 5000000});  
 
   console.log("Vesting: ", Vesting.address)
